17d76c737cc1f240fa597d5024f2b3cbbe0ce25b
DY <otamereisrael8@gmail.com>
2025-07-09 15:21:21 +0100

    i am working on the add to cart functionality Rn

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,40 +1,52 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/ProductSlice";
 function Home() {
-    const [datas, setDatas] = useState([])
-    const [load, setLoad] = useState(true)
-    const res = axios('https://fakestoreapi.com/products')
-res.then((items)=>{
-setDatas(items.data)
-setLoad(false)
-})
-console.log(datas);
+  const [datas, setDatas] = useState([]);
+  const [load, setLoad] = useState(true);
 
+  useEffect(() => {
+    const res = axios("https://fakestoreapi.com/products");
+    res.then((items) => {
+      setDatas(items.data);
+      setLoad(false);
+    });
+  }, []);
+
+  const dispatch = useDispatch();
   return (
     <>
-    <div  className='cont'>
-        {
-           load ? (<h1>loading....</h1>) : datas.map((rend, index)=>{
-            return(
-                <ul className='list'>
-                    <li><img className='img' src={rend.image} alt="jpeg" /></li>
-                    <li><p>{rend.title}</p></li>
-                    <li>{rend.category}</li>
-                 
-                    <li><h3>{rend.price}</h3></li>
-                     <button> Add to Cart</button>
-                    <h5>{rend.id}</h5>
-                   
-                </ul>
-                
-            )
+      <div className="cont">
+        {load ? (
+          <h1>loading....</h1>
+        ) : (
+          datas.map((rend, index) => {
+            return (
+              <ul className="list">
+                <li>
+                  <img className="img" src={rend.image} alt="jpeg" />
+                </li>
+                <li>
+                  <p>{rend.title}</p>
+                </li>
+                <li>{rend.category}</li>
 
-           })
-        }
-    </div>
+                <li>
+                  <h3>{rend.price}</h3>
+                </li>
+                <button onClick={() => dispatch(addToCart(rend))}>
+                  {" "}
+                  Add to Cart
+                </button>
+                <h5>{rend.id}</h5>
+              </ul>
+            );
+          })
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
